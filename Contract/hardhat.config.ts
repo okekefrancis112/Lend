25dfbea61b38938b985d1a265617efdaaa9958e8
Dev_francis <okeke98@gmail.com>
2023-02-13 23:03:18 +0100

    config added

--- Contract/hardhat.config.ts
+++ Contract/hardhat.config.ts
@@ -1,8 +1,31 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config({path: ".env"});
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+      // blockConfirmations: 6,
+    },
+    mumbai: {
+      url: MUMBAI_RPC_URL,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
+  solidity: {
+    compilers: [{ version: "0.8.9" }, { version: "0.6.6" }],
+  },
 };
-
-export default config;
